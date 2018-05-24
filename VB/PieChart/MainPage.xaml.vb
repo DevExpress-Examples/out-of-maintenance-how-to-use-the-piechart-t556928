9b8e4f161363a4599fe00b500a97cffea66884b0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-24 16:40:13 +0300

    initial commit

---
+++ VB/PieChart/MainPage.xaml.vb
@@ -0,0 +1,29 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.IO
+Imports System.Linq
+Imports System.Runtime.InteropServices.WindowsRuntime
+Imports Windows.Foundation
+Imports Windows.Foundation.Collections
+Imports Windows.UI.Xaml
+Imports Windows.UI.Xaml.Controls
+Imports Windows.UI.Xaml.Controls.Primitives
+Imports Windows.UI.Xaml.Data
+Imports Windows.UI.Xaml.Input
+Imports Windows.UI.Xaml.Media
+Imports Windows.UI.Xaml.Navigation
+
+' The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+Namespace PieChart
+    ''' <summary>
+    ''' An empty page that can be used on its own or navigated to within a Frame.
+    ''' </summary>
+    Public NotInheritable Partial Class MainPage
+        Inherits Page
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
+End Namespace
